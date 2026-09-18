561a800ee53312367e6ef6201d1b8ea42ba32113
revanselebini0-star <revan.seleb.ini0@gmail.com>
2026-09-18 01:21:37 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/react-swc'
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: '/vanvanmusicapp-2.1/', 
+  base: '/vanmusicapp-2.1/', // Tambahkan baris ini!
 })
